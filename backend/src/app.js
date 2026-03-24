4486adcae1280dcee9e5bcb0f0a6209534f73184
SubastralAlok <alokkumaryadav241004@gmail.com>
2026-03-24 13:59:06 +0530

    used cors in backend

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,5 +1,6 @@
 import express from "express";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 import authRouter from "./routes/auth.routes.js";
 import songRouter from "./routes/song.routes.js";
 import historyRouter from "./routes/history.routes.js";
@@ -10,6 +11,10 @@ const app = express();
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(cookieParser());
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+}));
 
 app.use("/api/auth", authRouter);
 app.use("/api/songs", songRouter);
